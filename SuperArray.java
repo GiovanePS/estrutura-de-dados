9cb994edc3164551698fa1d7fefe8d16472768a0
Giovane Pimentel de Sousa <giovanespace@gmail.com>
2023-08-13 21:06:14 -0300

    superArray

---
+++ SuperArray.java
@@ -0,0 +1,36 @@
+public class SuperArray {
+    private Integer inicio;
+    private Integer fim;
+    private Integer tamanho;
+    private Integer[] superArray;
+    
+    public SuperArray(Integer inicio, Integer fim) {
+        this.inicio = inicio;
+        this.fim = fim;
+        this.tamanho = fim - inicio + 1;
+        this.superArray = new Integer[tamanho];
+    }
+
+    public Integer acessar(Integer posicao) {
+        this.limiteArrayException(posicao);
+        return this.superArray[posicao-this.inicio];
+    }
+
+    public void atribuir(Integer posicao, Integer valor) {
+        this.limiteArrayException(posicao);
+        this.superArray[posicao-this.inicio] = valor;
+    }
+
+    private void limiteArrayException(Integer posicao) {
+        try {
+            if (posicao < this.inicio || posicao > this.fim) {
+                throw new ArrayIndexOutOfBoundsException(
+                    String.format("Posição %d fora dos limites do array (%d..%d).",
+                    posicao, this.inicio, this.fim)
+                );
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
